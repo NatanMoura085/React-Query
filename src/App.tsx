df12e8d22035f129251ee220d8be7efebbf748e8
Natan <mouranatan933@gmail.com>
2023-09-27 21:01:03 -0300

    fix: cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 
-
 import { useDataHandler } from './hooks/useDataHandler';
 
 function App() {
@@ -16,14 +15,15 @@ function App() {
     return <div>Erro ao carregar os personagens.</div>;
   }
 
-  // Agora, data contém a lista de personagens da série
+ 
   return (
     <div>
-      <h2>Personagens de Rick and Morty</h2>
+    
       <ul>
         {data.results.map((character:any) => (
           <li key={character.id}>
             <h3>{character.name}</h3>
+            <img src={character.image} />
             <p>Status: {character.status}</p>
             <p>Espécie: {character.species}</p>
           
